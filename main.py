3d73fec27ec14a06109504fcdb9fc3eec2ccf74b
KaterynaDanylchenko <108829987+KaterynaDanylchenko@users.noreply.github.com>
2022-10-05 16:00:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 def parse(query: str) -> dict:
-    return {}
+    find_symbol = query.rfind('/')
+    string_to_check = query[find_symbol + 1:]
+    fix_symbol = string_to_check.rfind('?')
+    slice_to_check_final = string_to_check[fix_symbol + 1:]
+    replace_str = slice_to_check_final.replace('&', ' ')
+    list_to_compare = replace_str.split()
+    dict_final = {}
+    for i in list_to_compare:
+        position = i.rfind('=')
+        key = i[0:position]
+        value = (i[position + 1:])
+        dict_final[key] = value
+    return dict_final
 
-
-if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('https://example.com/path/to/name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/&name=ferret') == {'name': 'ferret'}
+    assert parse('https://example.com/path/to/=ferret') == {'': 'ferret'}
+    assert parse('https://example.com/path/to/page?') == {}
+    assert parse('https://example.com/path/to/&') == {}
+    assert parse('https://example.com/path/to/page?{name}=ferret&color==purple&') == {'{name}': 'ferret', 'color=': 'purple'}
+    assert parse('http://example.com/name=') == {'name': ''}
+    assert parse('http://example.com/?colour=None') == {'colour': 'None'}
+    assert parse('https://example.com/path/to/&name=ferret&') == {'name': 'ferret'}
+    assert parse('https://example.com/path/to/name=Dima age=28 city=Kiev') == {'name': 'Dima', 'age': '28', 'city': 'Kiev'}
+
+
+
+
 
 
 def parse_cookie(query: str) -> dict:
